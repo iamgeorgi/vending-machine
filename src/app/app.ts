3cf108db8e33e653140f71ae28c9f205a49bfd79
iamgeorgi <georgipeetrov@gmail.com>
2026-09-23 13:57:32 +0300

    add page navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, effect, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink,RouterLinkActive, MatButtonModule],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
 })
-export class App {
-}
+export class App {}
